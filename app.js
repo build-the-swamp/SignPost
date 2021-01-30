3f71677115e2343830f6975990264bc367968c49
iadamalad <53870970+iadamalad@users.noreply.github.com>
2021-01-30 14:30:51 -0800

    Added sign language options

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const fileContents = fs.readFileSync("./ASL.json", "utf8");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const request = require("request");
-//const axios = require("axios");
+
+const TextToGifSign = require("./commands/gif-spell");
+const TextToFingerSign = require("./commands/finger-spell");
 
 let data = {};
 
@@ -21,58 +23,45 @@ client.once("ready", () => {
 client.on("message", async (message) => {
   if (message.author.bot) return;
 
-  const command = "translate";
   if (
-    message.content.startsWith(prefix + command) ||
+    message.content.startsWith(prefix + "translate") ||
+    message.content.startsWith(prefix + "letter") ||
     message.channel.id === "805169855396446249"
   ) {
-    let msg = "";
-    if (message.content.startsWith(prefix + command))
-      msg = message.content.slice(command.length + 2).toLowerCase();
-    else msg = message.content;
+    let letterOnly = false;
+    msg_split = message.content.split(" "); // splits the sentence into different parts
+    let firstItem = msg_split[0];
 
-    msg_split = msg.split(" "); // splits the sentence into different parts
+    if (firstItem === "/translate" || firstItem === "/letter") {
+      msg_split.shift();
+      letterOnly = firstItem === "/letter";
+    } else if (firstItem.startsWith(prefix)) return;
 
-    console.log(msg_split);
     for (let i = 0; i < msg_split.length; i++) {
-        let is_valid = await IsValidLink(msg_split[i].toLowerCase())
-        console.log(is_valid)
-        if(is_valid)
-        {
-            TextToGifSign(msg_split[i])                                                         // If there is a gif for a specific word send it
-        }
-        else
-        {
-            TextToFingerSign(msg_split[i], message)                                             // If there is no gif send a word letter by letter
-        }
-        
+      let is_valid = await IsValidLink(msg_split[i].toLowerCase());
+      if (is_valid && !letterOnly) {
+        TextToGifSign.execute(msg_split[i], message); // If there is a gif for a specific word send it
+      } else {
+        TextToFingerSign.execute(msg_split[i], message, data); // If there is no gif send a word letter by letter
+      }
     }
   }
 });
 
-async function IsValidLink(link)
-{
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            request("https://www.handspeak.com/word/" + link[0] + "/" + link +".mp4", async function (error, response, body) { // we request a webpage so we know if a link exists
-                if (response.statusCode == 200) {
-                    resolve(true)
-                }
-                else
-                    resolve(false)
-            });
-        }, 1500)
-    })
+async function IsValidLink(link) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      request(
+        "https://www.handspeak.com/word/" + link[0] + "/" + link + ".mp4",
+        async function (error, response, body) {
+          // we request a webpage so we know if a link exists
+          if (response.statusCode == 200) {
+            resolve(true);
+          } else resolve(false);
+        }
+      );
+    }, 1500);
+  });
 }
 
 client.login(token);
-
-/*
-
-For each word we see if we have a gif for it?
-
-If we do then we give it
-
-If we dont we sign it andddddd we ask if they have a gif for it?
-
-*/
--- commands/finger-spell.js
+++ commands/finger-spell.js
@@ -1,11 +1,13 @@
-function TextToFingerSign(msg, message)
-{
-    let fin_message = ""
+module.exports = {
+  name: "Letter Spell",
+  description: "Convert text to direct letters",
+  execute(msg, message, data) {
+    let fin_message = "";
     for (let j = 0; j < msg.length; j++) {
-        if (data[msg[j]])
-            fin_message = fin_message + " " + data[msg[j]];
+      if (data[msg[j]]) fin_message = fin_message + " " + data[msg[j]];
     }
     message.channel.send(msg);
     message.channel.send(fin_message); // sends the final message
     fin_message = ""; // resets the message so there are no repetition
-}
+  },
+};
