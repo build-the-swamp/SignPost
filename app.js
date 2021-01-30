d32ffe7d28b3182f60b522eb336d3dc3375e513b
iadamalad <53870970+iadamalad@users.noreply.github.com>
2021-01-30 11:13:24 -0800

    Added bot detection and talk in translate channel.

--- app.js
+++ app.js
@@ -1,51 +1,73 @@
-const {prefix, token} = require('./config.json');
-const fs = require('fs')
-const fileContents = fs.readFileSync('./ASL.json', 'utf8')
-const Discord = require('discord.js');
+const { prefix, token } = require("./config.json");
+const fs = require("fs");
+const fileContents = fs.readFileSync("./ASL.json", "utf8");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const request = require("request")
+const request = require("request");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 let data = {};
 
-try{
-    data = JSON.parse(fileContents)
-} catch(err){console.log(err)}
+try {
+  data = JSON.parse(fileContents);
+} catch (err) {
+  console.log(err);
+}
 
-client.once('ready', () => {
-    console.log('RUNNING!');
+client.once("ready", () => {
+  console.log("RUNNING!");
 });
 
+client.on("message", async (message) => {
+  if (message.author.bot) return;
 
-client.on('message', async message => {
-    if (!message.content.startsWith(prefix + "ra") ) return;
+  const command = "translate";
+  if (
+    message.content.startsWith(prefix + command) ||
+    message.channel.id === "805145031547420703"
+  ) {
+    let msg = "";
+    if (message.content.startsWith(prefix + command))
+      msg = message.content.slice(command.length + 2).toLowerCase();
+    else msg = message.content;
 
-    msg = message.content.slice(4).toLowerCase()
-    msg_split = msg.split(" ") // splits the sentence into different parts
+    msg_split = msg.split(" "); // splits the sentence into different parts
 
-    let fin_message = ""
-    console.log(msg_split)
-    for(let i = 0; i < msg_split.length; i++)
-    {
-        await request("https://www.handspeak.com/word/" + msg_split[i][0] + "/" + msg_split[i] + ".mp4", async function(error, response, body){
-            console.error('error:', error);
-            console.log('statusCode:', response && response.statusCode);
-            if(response.statusCode == 200){
-                await message.channel.send(msg_split[i])
-                await message.channel.send("https://www.handspeak.com/word/" +  msg_split[i][0] + "/" + msg_split[i] + ".mp4");
+    let fin_message = "";
+    console.log(msg_split);
+    for (let i = 0; i < msg_split.length; i++) {
+      await request(
+        "https://www.handspeak.com/word/" +
+          msg_split[i][0] +
+          "/" +
+          msg_split[i] +
+          ".mp4",
+        async function (error, response, body) {
+          console.log("statusCode:", response && response.statusCode);
+          if (response.statusCode == 200) {
+            message.channel.send(msg_split[i]);
+            message.channel.send(
+              "https://www.handspeak.com/word/" +
+                msg_split[i][0] +
+                "/" +
+                msg_split[i] +
+                ".mp4"
+            );
+          } else {
+            for (let j = 0; j < msg_split[i].length; j++) {
+              if (data[msg_split[i][j]])
+                fin_message = fin_message + " " + data[msg_split[i][j]];
             }
-            else{
-                for(let j = 0;j < msg_split[i].length; j++){
-                    if(data[msg_split[i][j]])
-                        fin_message = fin_message + " " + data[msg_split[i][j]]
-                }
-                await message.channel.send(msg_split[i])
-                await message.channel.send(fin_message) // sends the final message
-                fin_message = "" // resets the message so there are no repetition
-            }
-        });
+            message.channel.send(msg_split[i]);
+            amessage.channel.send(fin_message); // sends the final message
+            fin_message = ""; // resets the message so there are no repetition
+          }
+        }
+      );
     }
-})
-
+  }
+});
 
 client.login(token);
 
