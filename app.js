53259e5898cb50598b886032f1c92e59c2d204b0
iadamalad <53870970+iadamalad@users.noreply.github.com>
2021-01-31 00:41:23 -0800

    Added Audio Recognition

--- app.js
+++ app.js
@@ -5,6 +5,38 @@ const fileContents = fs.readFileSync("./ASL.json", "utf8");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const request = require("request");
+const speech = require("@google-cloud/speech");
+
+const { Transform, Readable } = require("stream");
+const SILENCE = Buffer.from([0xf8, 0xff, 0xfe]);
+
+class Silence extends Readable {
+  _read() {
+    this.push(SILENCE);
+    this.destroy();
+  }
+}
+
+function convertBufferTo1Channel(buffer) {
+  const convertedBuffer = Buffer.alloc(buffer.length / 2);
+
+  for (let i = 0; i < convertedBuffer.length / 2; i++) {
+    const uint16 = buffer.readUInt16LE(i * 4);
+    convertedBuffer.writeUInt16LE(uint16, i * 2);
+  }
+
+  return convertedBuffer;
+}
+
+class ConvertTo1ChannelStream extends Transform {
+  constructor(source, options) {
+    super(options);
+  }
+
+  _transform(data, encoding, next) {
+    next(null, convertBufferTo1Channel(data));
+  }
+}
 
 const TextToGifSign = require("./commands/gif-spell");
 const TextToFingerSign = require("./commands/finger-spell");
@@ -32,22 +64,79 @@ client.once("ready", () => {
 client.on("message", async (message) => {
   if (message.author.bot) return;
 
+  if (
+    message.content.startsWith(prefix + "talk") &&
+    message.member.voice.channel
+  ) {
+    const google_client = new speech.SpeechClient();
+    const connection = await message.member.voice.channel.join();
+    const receiver = connection.createReciever;
+    connection.play(new Silence(), { type: "opus" }); // plays silent audio in the background
+
+    connection.on("speaking", async (user, speaking) => {
+      if (!speaking) {
+        return;
+      }
+      //create 16-bit, stereo, 48Khz audio stream
+      const audioStream = connection.receiver.createStream(user, {
+        mode: "pcm",
+      });
+      const requestConfig = {
+        encoding: "LINEAR16",
+        sampleRateHertz: 48000,
+        languageCode: "en-US",
+      };
+      const request = {
+        config: requestConfig,
+      };
+      const recognizeStream = google_client
+        .streamingRecognize(request)
+        .on("error", console.error)
+        .on("data", async (response) => {
+          const transcription = response.results
+            .map((result) => result.alternatives[0].transcript)
+            .join("\n")
+            .toLowerCase();
+
+          let trans_message = "";
+          trans_message = transcription;
+
+          let msg_split = trans_message.split(" ");
+          console.log("msg_split = ", msg_split);
+          if (msg_split == "") return;
+          for (let i = 0; i < msg_split.length; i++) {
+            let is_valid = await IsValidLink(g_split[i].toLowerCase()); // goes and checks if the link were guessing is valid
+
+            if (is_valid) {
+              TextToGifSign.execute(msg_split, message); // If there is a gif for a specific word send it
+            } else if (!is_valid) {
+              if (gif_db[msg_split[i]] != undefined) {
+                // checking if we have a gif in our database
+                message.channel.send(gif_db[msg_split[i]]);
+              } else TextToFingerSign.execute(msg_split[i], message, data); // If there is no gif in both our data base and our gif repo then send a word letter by letter
+            } else {
+              TextToFingerSign.execute(msg_split[i], message, data); // If the user wants letter by letter instead of gifs
+            }
+          }
+
+          console.log(`Transcription: ${transcription}`);
+        });
+
+      //console.log(recognizeStream);
+
+      const convertTo1ChannelStream = new ConvertTo1ChannelStream();
+
+      audioStream.pipe(convertTo1ChannelStream).pipe(recognizeStream);
+    }); //end connection.on()
+    console.log(message.member.user.id);
+  }
+
   if (message.content.startsWith(prefix + "add")) {
     msg_split = message.content.split(" "); // splits the sentence into different parts
     msg_split.shift();
 
     Add.execute(msg_split, message, client);
   }
-  
-  // console.log("Author id: ", message.author.id);
-  // console.log("Admin: ", admin);
-
-  // if (message.author.id === admin) {
-  //   console.log("Confirmeeeeee");
-  //   msg_split = message.content.split(" "); // splits the sentence into different parts
-  //   Permit.execute(msg_split, message);
-  //   return;
-  // }
 
   if (
     message.content.startsWith(prefix + "translate") ||
@@ -64,35 +153,20 @@ client.on("message", async (message) => {
     } else if (firstItem.startsWith(prefix)) return;
 
     for (let i = 0; i < msg_split.length; i++) {
-      let is_valid = await IsValidLink(msg_split[i].toLowerCase());
+      let is_valid = await IsValidLink(msg_split[i].toLowerCase()); // goes and checks if the link were guessing is valid
+
       if (is_valid && !letterOnly) {
         TextToGifSign.execute(msg_split[i], message); // If there is a gif for a specific word send it
-      } else if(!is_valid && !letterOnly) {
-        if(gif_db[msg_split[i]] != undefined)
-          message.channel.send(gif_db[msg_split[i]])
-        else
-          TextToFingerSign.execute(msg_split[i], message, data); // If there is no gif send a word letter by letter
-      }else{
-        TextToFingerSign.execute(msg_split[i], message, data); // If there is no gif send a word letter by letter
+      } else if (!is_valid && !letterOnly) {
+        if (gif_db[msg_split[i]] != undefined)
+          message.channel.send(gif_db[msg_split[i]]);
+        // checking if we have a gif in our database
+        else TextToFingerSign.execute(msg_split[i], message, data); // If there is no gif in both our data base and our gif repo then send a word letter by letter
+      } else {
+        TextToFingerSign.execute(msg_split[i], message, data); // If the user wants letter by letter instead of gifs
       }
     }
   }
 });
-
-async function IsValidLink(link) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      request(
-        "https://www.handspeak.com/word/" + link[0] + "/" + link + ".mp4",
-        async function (error, response, body) {
-          // we request a webpage so we know if a link exists
-          if (response.statusCode == 200) {
-            resolve(true);
-          } else resolve(false);
-        }
-      );
-    }, 1500);
-  });
-}
 
 client.login(token);
--- commands/gif-spell.js
+++ commands/gif-spell.js
@@ -15,8 +15,9 @@ module.exports = {
   name: "Gif Spell",
   description: "Sends gif",
   execute(msg, message) {
+    msg = msg.toLowerCase();
     message.channel.send(
-      "https://www.handspeak.com/word/" + msg[0] + "/" + msg + ".mp4"
+      msg + " https://www.handspeak.com/word/" + msg[0] + "/" + msg + ".mp4"
     );
   },
 };
